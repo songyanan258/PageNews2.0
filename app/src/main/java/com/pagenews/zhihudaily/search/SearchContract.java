1e7296bed62899f73b7bcd4748f9e02116d651ad
songyanan <haiyan.xu.vip@gmail.com>
2017-04-07 17:37:31 +0800

    the cmoplete

---
+++ app/src/main/java/com/pagenews/zhihudaily/search/SearchContract.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2017 lizhaotailang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pagenews.zhihudaily.search;
+
+import com.pagenews.zhihudaily.BasePresenter;
+import com.pagenews.zhihudaily.BaseView;
+import com.pagenews.zhihudaily.bean.BeanType;
+import com.pagenews.zhihudaily.bean.DoubanMomentNews;
+import com.pagenews.zhihudaily.bean.GuokrHandpickNews;
+import com.pagenews.zhihudaily.bean.ZhihuDailyNews;
+
+import java.util.ArrayList;
+
+/**
+ * Created by lizhaotailang on 2016/12/25.
+ */
+
+public interface SearchContract {
+
+    interface View extends BaseView<Presenter> {
+
+        void showResults(ArrayList<ZhihuDailyNews.Question> zhihuList,
+                         ArrayList<GuokrHandpickNews.result> guokrList,
+                         ArrayList<DoubanMomentNews.posts> doubanList,
+                         ArrayList<Integer> types);
+
+        void showLoading();
+
+        void stopLoading();
+
+    }
+
+    interface Presenter extends BasePresenter {
+
+        void loadResults(String queryWords);
+
+        void startReading(BeanType type, int position);
+
+    }
+
+}
